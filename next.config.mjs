29e7456ee041d9b6595bf32b3b9227a903bf1a90
Matias Crivolotti <matioav@gmail.com>
2025-01-29 11:09:43 +0200

    cleanup next config

--- next.config.mjs
+++ next.config.mjs
@@ -13,16 +13,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  experimental: {
-    turbo: {
-      rules: {
-        "*.svg": {
-          loaders: ["@svgr/webpack"],
-          as: "*.js",
-        },
-      },
-    },
-  },
 };
 
 export default withNextIntl(nextConfig);
